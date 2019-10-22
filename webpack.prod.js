cc244de8be647638ce081162adac2f64ebc415d7
Gustavo Calonico <500744@soa.ad.pwcinternal.com>
2019-10-22 17:33:16 -0300

    Add dev configurations

---
+++ webpack.prod.js
@@ -0,0 +1,17 @@
+const webpack = require('webpack')
+const merge = require('webpack-merge')
+const common = require('./webpack.common.js')
+
+module.exports = merge(common, {
+	plugins: [
+		new webpack.optimize.UglifyJsPlugin({
+			sourceMap: true,
+			uglifyOptions: { ecma: 8 },
+		}),
+		new webpack.DefinePlugin({
+			'process.env': {
+				'NODE_ENV': JSON.stringify('production'),
+			},
+		})
+	],
+})
