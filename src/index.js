34c8617b1ed93fd7b5902d1aa72c7eb33c8b30d6
gusjasponde <gustavocalonico@gmail.com>
2019-10-23 17:15:33 -0300

    Add multi-language support

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { render } from 'react-dom'
 import { Provider } from 'react-redux'
 
+import './dictionary'
 import { AppContainer } from './containers'
 import configureStore from './store'
 
