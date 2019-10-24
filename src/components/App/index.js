eed61140cf495cd5e5553f6b8a19dbc6bedc4060
Gustavo Calonico <500744@soa.ad.pwcinternal.com>
2019-10-24 17:53:06 -0300

    Change layout

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,27 +1,29 @@
 import React from 'react'
-import clsx from 'clsx'
 import PropTypes from 'prop-types'
+import AppBar from '@material-ui/core/AppBar'
+import Button from '@material-ui/core/Button'
+import Card from '@material-ui/core/Card'
+import CardActions from '@material-ui/core/CardActions'
+import CardContent from '@material-ui/core/CardContent'
+import CardHeader from '@material-ui/core/CardHeader'
 import CssBaseline from '@material-ui/core/CssBaseline'
-import Link from '@material-ui/core/Link'
-import Drawer from '@material-ui/core/Drawer'
 import Grid from '@material-ui/core/Grid'
+import StarIcon from '@material-ui/icons/StarBorder'
+import Toolbar from '@material-ui/core/Toolbar'
 import Typography from '@material-ui/core/Typography'
-import Divider from '@material-ui/core/Divider'
-import List from '@material-ui/core/List'
-import IconButton from '@material-ui/core/IconButton'
-import ChevronLeftIcon from '@material-ui/icons/ChevronLeft'
+import Link from '@material-ui/core/Link'
 import { makeStyles } from '@material-ui/core/styles'
-import mainListItems from './listItems'
-
-const drawerSize = 240
+import Container from '@material-ui/core/Container'
+import Box from '@material-ui/core/Box'
+import { useTranslation } from 'react-i18next'
 
 function Copyright() {
 	return (
 		<Typography variant="body2" color="textSecondary" align="center">
-			{'Copyright © '}
+			{'Made with ♥ and '}
 			<Link color="inherit" href="https://material-ui.com/">
-				Your Website
-			</Link>{' '}
+				MaterialUI
+			</Link>{', '}
 			{new Date().getFullYear()}
 			{'.'}
 		</Typography>
@@ -29,91 +31,175 @@ function Copyright() {
 }
 
 const useStyles = makeStyles(theme => ({
-	root: {
-		height: '100vh',
-	},
-	toolbar: {
-		paddingRight: 24,
-	},	
-	sidebarPaper: {
-		position: 'relative',
-		whiteSpace: 'nowrap',
-		width: drawerSize,
-	},
-	toolbarIcon: {
-		display: 'flex',
-		alignItems: 'center',
-		justifyContent: 'flex-end',
-		padding: '0 8px',
-		...theme.mixins.toolbar,
-	},
-	paper: {
-		padding: theme.spacing(2),
-		display: 'flex',
-		overflow: 'auto',
-		flexDirection: 'column',
-	},
-	drawerPaper: {
-		position: 'relative',
-		whiteSpace: 'nowrap',
-		width: drawerSize,
-		transition: theme.transitions.create('width', {
-			easing: theme.transitions.easing.sharp,
-			duration: theme.transitions.duration.enteringScreen,
-		}),
-	},
-	drawerPaperClose: {
-		overflowX: 'hidden',
-		transition: theme.transitions.create('width', {
-			easing: theme.transitions.easing.sharp,
-			duration: theme.transitions.duration.leavingScreen,
-		}),
-		width: theme.spacing(7),
-		[theme.breakpoints.up('sm')]: {
-			width: theme.spacing(9),
+	'@global': {
+		body: {
+			backgroundColor: theme.palette.common.white,
+		},
+		ul: {
+			margin: 0,
+			padding: 0,
+		},
+		li: {
+			listStyle: 'none',
 		},
 	},
-	avatar: {
-		margin: theme.spacing(1),
-		backgroundColor: theme.palette.secondary.main,
+	root: {
+		display: 'flex'
 	},
-	
+	appBar: {
+		borderBottom: `1px solid ${theme.palette.divider}`,
+	},
+	toolbar: {
+		flexWrap: 'wrap',
+	},
+	toolbarTitle: {
+		flexGrow: 1,
+	},
+	link: {
+		margin: theme.spacing(1, 1.5),
+	},
+	heroContent: {
+		padding: theme.spacing(10, 0, 8),
+	},
+	cardHeader: {
+		backgroundColor: theme.palette.grey[200],
+	},
+	cardProject: {
+		display: 'flex',
+		justifyContent: 'center',
+		alignItems: 'baseline',
+		marginBottom: theme.spacing(2),
+	},
+	footer: {
+		borderTop: `1px solid ${theme.palette.divider}`,
+		marginTop: theme.spacing(8),
+		paddingTop: theme.spacing(3),
+		paddingBottom: theme.spacing(3),
+		[theme.breakpoints.up('sm')]: {
+			paddingTop: theme.spacing(6),
+			paddingBottom: theme.spacing(6),
+		},
+	},
 }))
+  
+const projects = [
+	{
+		title: 'Project',
+		description: ['Description', 'C++', 'Bitcoin', 'IPFS'],
+		buttonText: '',
+		buttonVariant: 'outlined',
+	}
+]
 
-let App = ({ drawerOpen, changeState }) => {
+let App = () => {
+	const { t } = useTranslation()
+	let text = {
+		home: t('home')
+	}
 	const classes = useStyles()
-	const listItems = mainListItems()
 
 	return(
 		<Grid container component="main" className={classes.root}>
 			<CssBaseline />
-			<Drawer
-				variant="permanent"
-				classes={{
-					paper: clsx(classes.drawerPaper, !open && classes.drawerPaperClose),
-				}}
-				open={drawerOpen}
-			>
-				<div className={classes.toolbarIcon}>
-					<IconButton onClick={() => changeState({drawerOpen: !drawerOpen})}>
-						<ChevronLeftIcon />
-					</IconButton>
-				</div>
-				<Divider />
-				<List>{listItems}</List>
-			</Drawer>
-			<Grid item xs={9} sm={9} md={9} >
-				<div className={classes.paper}>
-				</div>
-				<Copyright/>
-			</Grid>
+			<AppBar position="static" color="default" elevation={0} className={classes.appBar}>
+				<Toolbar className={classes.toolbar}>
+					<Typography variant="h6" color="inherit" noWrap className={classes.toolbarTitle}>
+					</Typography>
+					<nav>
+						<Link variant="button" color="textPrimary" href="#" className={classes.link}>
+							{text.home.sections.about}
+						</Link>
+						<Link variant="button" color="textPrimary" href="#" className={classes.link}>
+							{text.home.sections.projects}
+						</Link>
+						<Link variant="button" color="textPrimary" href="#" className={classes.link}>
+							{text.home.sections.contact}
+						</Link>
+					</nav>
+				</Toolbar>
+			</AppBar>
+			<Container component="main">
+				{/* Hero unit */}
+				<Container maxWidth="md" className={classes.heroContent}>
+					<Typography component="h1" variant="h2" align="center" color="textPrimary" gutterBottom>
+						{text.home.name}
+					</Typography>
+					<Typography variant="h5" align="center" color="textSecondary" component="p" gutterBottom>
+						{text.home.sub}
+					</Typography>
+					<Typography align="center" color="textSecondary" component="p">
+						{text.home.description}
+					</Typography>
+				</Container>
+				<Container maxWidth="md" component="main">
+					<Grid container spacing={5} >
+						{projects.map(project => (
+							// Enterprise card is full width at sm breakpoint
+							<Grid item key={project.title} xs={12} sm={project.title === 'Enterprise' ? 12 : 6} md={4}>
+								<Card>
+									<CardHeader
+										title={project.title}
+										subheader={project.subheader}
+										titleTypographyProps={{ align: 'center' }}
+										subheaderTypographyProps={{ align: 'center' }}
+										action={project.title === 'Pro' ? <StarIcon /> : null}
+										className={classes.cardHeader}
+									/>
+									<CardContent>
+										<ul>
+											{project.description.map(line => (
+												<Typography component="li" variant="subtitle1" align="center" key={line}>
+													{line}
+												</Typography>
+											))}
+										</ul>
+									</CardContent>
+									<CardActions>
+										<Button fullWidth variant={project.buttonVariant} color="primary">
+											{project.buttonText}
+										</Button>
+									</CardActions>
+								</Card>
+							</Grid>
+						))}
+					</Grid>
+				</Container>
+				{/* End hero unit */}
+				{/* Footer */}
+				<Container maxWidth="md" component="footer" className={classes.footer}>
+					<Grid container justify="space-evenly">
+						{text.home.footer.map(footer => (
+							<Grid item xs={1} sm={12} key={footer.title}>
+								<Typography variant="h6" color="textPrimary" gutterBottom>
+									{footer.title}
+								</Typography>
+								<ul>
+									{footer.description.map(item => (
+										<li key={item}>
+											<Link href="#" variant="subtitle1" color="textSecondary">
+												{item}
+											</Link>
+										</li>
+									))}
+								</ul>
+							</Grid>
+						))}
+					</Grid>
+					<Box mt={5}>
+						<Copyright />
+					</Box>
+				</Container>
+				{/* End footer */}
+			</Container>
+			
+			
 		</Grid>
 	)
 }
 
 App.propTypes = {
-	drawerOpen: PropTypes.bool,
-	changeState: PropTypes.func
+	state: PropTypes.object,
+	setState: PropTypes.func
 }
 
 export default App
--- src/containers/App/index.js
+++ src/containers/App/index.js
@@ -1,5 +1,4 @@
 import React from 'react'
-import PropTypes from 'prop-types'
 
 import App from '../../components/App'
 import { connect } from 'react-redux'
@@ -8,28 +7,12 @@ class AppContainer extends React.Component {
 
 	constructor(props) {
 		super(props)
-		this.state = {
-			drawerOpen: true,
-		}
-
-		this.props = {
-			changeState: this.changeState.bind(this)
-		}
-	}
-
-	static propTypes = {
-		changeState: PropTypes.func
-	}
-
-	changeState(object) {
-		this.setState(object)
+		this.setState = this.setState.bind(this)
 	}
 
 	render() {
 		return (
-			<App
-				drawerOpen={this.state.drawerOpen}
-				changeState={this.props.changeState}/>
+			<App/>
 		)
 	}
 }
