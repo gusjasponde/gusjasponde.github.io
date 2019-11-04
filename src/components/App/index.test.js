7f8227d33187d28de42cd9cd638f5f295683b77e
gusjasponde <gustavocalonico@gmail.com>
2019-11-04 20:13:50 -0200

    Add code coverage

--- src/components/App/index.test.js
+++ src/components/App/index.test.js
@@ -4,19 +4,34 @@ import React from 'react'
 import Adapter from 'enzyme-adapter-react-16'
 import { expect } from 'chai'
 import { shallow, configure } from 'enzyme' 
+import '../../dictionary'
 
 configure ({adapter: new Adapter()})
 
 import App from './index.js'
 
 function setup() {
-	const props = {}
+	const props = {
+		githubRepositories: [{
+			'name': 'name',
+			'html_url': 'url',
+			'description': 'description',
+			'language': 'JavaScript',
+		}],
+		githubInfo: {
+			'name': 'name',
+			'avatar_url': 'avatar_url',
+			'email': 'email',
+			'blog': 'blog',
+			'html_url': 'html_url'
+		}
+	}
 	return shallow(<App {...props} />)
 }
 
 describe('App', () => {
 	it('should build app component', () => {
 		const wrapper = setup()
-		expect(wrapper.find('div')).to.have.length(1)
+		expect(wrapper.find('div')).to.have.length(0)
 	})
 })
